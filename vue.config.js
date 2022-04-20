72c1a5d6338951652d5a51803ca534320733a591
唐之胜 <48917726+Sanmer0312@users.noreply.github.com>
2022-04-20 16:51:08 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -16,4 +16,12 @@ module.exports = {
         }
     },
     publicPath: './',
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'title'
+                return args
+            })
+    }
 }
